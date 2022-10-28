450e36397676ccd3204ab2221f8af8f54c538984
Adrian Rojo <rojoadrian16@gmail.com>
2022-10-28 12:49:22 -0500

    created auth routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import Login from './components/pages/Login'
+import Profile from './components/pages/Profile'
+import Register from './components/pages/Register'
+import Navbar from './components/partials/Navbar'
+import EditProfile from './components/pages/EditProfile'
+import './App.css'
+import jwt_decode from 'jwt-decode'
+
 
 function App() {
+  // the currently logged in user will be stored up here in state
+  const [currentUser, setCurrentUser] = useState(localStorage.getItem('jwt')? jwt_decode(localStorage.getItem('jwt')): null)
+
+  // useEffect -- if the user navigates away form the page, we will log them back in
+  useEffect(() => {
+    // check to see if token is in storage
+    const token = localStorage.getItem('jwt')
+    if (token) {
+      // if so, we will decode it and set the user in app state
+      setCurrentUser(jwt_decode(token))
+    } else {
+      setCurrentUser(null)
+    }
+  }, []) // happen only once
+
+  // event handler to log the user out when needed
+  const handleLogout = () => {
+    // check to see if a token exists in local storage
+    if (localStorage.getItem('jwt')) {
+      // if so, delete it
+      localStorage.removeItem('jwt')
+      // set the user in the App state to be null
+      setCurrentUser(null)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <header>
+        <Navbar 
+          currentUser={currentUser}
+          handleLogout={handleLogout}
+        />
       </header>
-    </div>
+
+      <div className="App">
+        <Routes>
+          <Route 
+            path="/register"
+            element={<Register currentUser={currentUser} setCurrentUser={setCurrentUser} />}
+          />
+
+          <Route 
+            path="/"
+            element={<Login currentUser={currentUser} setCurrentUser={setCurrentUser} />}
+          />
+
+          {/* conditionally render auth locked routes */}
+          <Route 
+            path="/:username"
+            element={currentUser ? <Profile handleLogout={handleLogout} currentUser={currentUser} setCurrentUser={setCurrentUser} /> : <Navigate to="/" />}
+          />
+
+           <Route 
+            path="/:username/edit"
+            element={currentUser ? <EditProfile handleLogout={handleLogout}/> : <Navigate to="/" />}
+          />
+           <Route 
+            path="/*"
+            element={<Navigate to="/" />}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/partials/Navbar.jsx
@@ -0,0 +1,53 @@
+import { Link } from 'react-router-dom'
+
+
+export default function Navbar({ currentUser, handleLogout }) {
+	let username = ''
+
+	if(currentUser){
+		username = currentUser.username
+	}
+	const loggedIn = (
+		<nav className='navbar navbar-expand-md navbar-dark'>
+			<div className='container navBarCont'>
+				<button className='navbar-toggler ms-auto' type= 'button' data-bs-toggle='collapse' data-bs-target='#navmenu'>
+					<span className='navbar-toggler-icon'></span>
+				</button>
+
+					<div className='collapse navbar-collapse ms-auto' id='navmenu'>
+						<ul className='navbar-nav ms-auto'>
+							<li className='nav-item'>
+								{/* if the user is logged in... */}
+								<Link to="/" className='nav-link'>
+									<span onClick={handleLogout}>Logout</span>
+								</Link>
+							</li>
+
+							<li>
+								<Link to={`/${username}`} className='nav-link'>
+									Profile
+								</Link>
+							</li>
+						</ul>
+					</div>
+			</div>
+
+		</nav>
+		
+		
+		)
+		
+		
+		
+		const loggedOut = (
+			console.log('please sign in')
+	 )
+	 
+	 return (
+		 <div>
+				{/* user always sees this section */}
+				
+				{currentUser ? loggedIn : loggedOut}
+		</div>
+	)
+}
